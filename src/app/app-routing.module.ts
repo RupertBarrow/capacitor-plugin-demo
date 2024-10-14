e4fc45613b38729dc5d8520b566c4e45ee6a6eea
RupertBarrow <rupert.barrow@lilo.org>
2024-10-14 16:24:18 +0200

    fix: break down to simple Posthog reference implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,112 +8,9 @@ const routes: Routes = [
     pathMatch: 'full',
   },
   {
-    path: 'app-update',
-    loadChildren: () =>
-      import('./modules/app-update/app-update.module').then(
-        m => m.AppUpdatePageModule,
-      ),
-  },
-  {
-    path: 'android-battery-optimization',
-    loadChildren: () =>
-      import(
-        './modules/android-battery-optimization/android-battery-optimization.module'
-      ).then(m => m.AndroidBatteryOptimizationPageModule),
-  },
-  {
-    path: 'android-foreground-service',
-    loadChildren: () =>
-      import(
-        './modules/android-foreground-service/android-foreground-service.module'
-      ).then(m => m.AndroidForegroundServicePageModule),
-  },
-  {
-    path: 'background-task',
-    loadChildren: () =>
-      import('./modules/background-task/background-task.module').then(
-        m => m.BackgroundTaskPageModule,
-      ),
-  },
-  {
-    path: 'badge',
-    loadChildren: () =>
-      import('./modules/badge/badge.module').then(m => m.BadgePageModule),
-  },
-  {
-    path: 'bluetooth-low-energy',
-    loadChildren: () =>
-      import('./modules/bluetooth-low-energy/bluetooth-low-energy.module').then(
-        m => m.BluetoothLowEnergyPageModule,
-      ),
-  },
-  {
-    path: 'cloudinary',
-    loadChildren: () =>
-      import('./modules/cloudinary/cloudinary.module').then(
-        m => m.CloudinaryPageModule,
-      ),
-  },
-  {
-    path: 'datetime-picker',
-    loadChildren: () =>
-      import('./modules/datetime-picker/datetime-picker.module').then(
-        m => m.DatetimePickerPageModule,
-      ),
-  },
-  {
-    path: 'file-compressor',
-    loadChildren: () =>
-      import('./modules/file-compressor/file-compressor.module').then(
-        m => m.FileCompressorPageModule,
-      ),
-  },
-  {
-    path: 'file-opener',
-    loadChildren: () =>
-      import('./modules/file-opener/file-opener.module').then(
-        m => m.FileOpenerPageModule,
-      ),
-  },
-  {
-    path: 'file-picker',
-    loadChildren: () =>
-      import('./modules/file-picker/file-picker.module').then(
-        m => m.FilePickerPageModule,
-      ),
-  },
-  {
     path: 'home',
     loadChildren: () =>
       import('./modules/home/home.module').then(m => m.HomePageModule),
-  },
-  {
-    path: 'keep-awake',
-    loadChildren: () =>
-      import('./modules/keep-awake/keep-awake.module').then(
-        m => m.KeepAwakePageModule,
-      ),
-  },
-  {
-    path: 'live-update',
-    loadChildren: () =>
-      import('./modules/live-update/live-update.module').then(
-        m => m.LiveUpdatePageModule,
-      ),
-  },
-  {
-    path: 'managed-configurations',
-    loadChildren: () =>
-      import(
-        './modules/managed-configurations/managed-configurations.module'
-      ).then(m => m.ManagedConfigurationsPageModule),
-  },
-  {
-    path: 'photo-editor',
-    loadChildren: () =>
-      import('./modules/photo-editor/photo-editor.module').then(
-        m => m.PhotoEditorPageModule,
-      ),
   },
   {
     path: 'posthog',
@@ -121,49 +18,8 @@ const routes: Routes = [
       import('./modules/posthog/posthog.module').then(m => m.PosthogPageModule),
   },
   {
-    path: 'printer',
-    loadChildren: () =>
-      import('./modules/printer/printer.module').then(m => m.PrinterPageModule),
-  },
-  {
-    path: 'privacy-screen',
-    loadChildren: () =>
-      import('./modules/privacy-screen/privacy-screen.module').then(
-        m => m.PrivacyScreenPageModule,
-      ),
-  },
-  {
-    path: 'screen-orientation',
-    loadChildren: () =>
-      import('./modules/screen-orientation/screen-orientation.module').then(
-        m => m.ScreenOrientationPageModule,
-      ),
-  },
-  {
-    path: 'text-to-speech',
-    loadChildren: () =>
-      import('./modules/text-to-speech/text-to-speech.module').then(
-        m => m.TextToSpeechPageModule,
-      ),
-  },
-  {
-    path: 'torch',
-    loadChildren: () =>
-      import('./modules/torch/torch.module').then(m => m.TorchPageModule),
-  },
-  {
-    path: 'wifi',
-    loadChildren: () =>
-      import('./modules/wifi/wifi.module').then(m => m.WifiPageModule),
-  },
-  {
-    path: 'zip',
-    loadChildren: () =>
-      import('./modules/zip/zip.module').then(m => m.ZipPageModule),
-  },
-  {
     path: '**',
-    redirectTo: 'home',
+    redirectTo: '/posthog',
   },
 ];
 
--- src/app/modules/posthog/posthog.page.ts
+++ src/app/modules/posthog/posthog.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
-import { Posthog } from '@capawesome/capacitor-posthog';
+//import { Posthog } from '@capawesome/capacitor-posthog';
+import { posthog } from 'posthog-js';
 
 @Component({
   selector: 'app-posthog',
@@ -23,62 +24,87 @@ export class PosthogPage {
   }
 
   public async alias(): Promise<void> {
+    /*
     await Posthog.alias({
       alias: 'alias',
     });
+    */
   }
 
   public async capture(): Promise<void> {
+    /*
     await Posthog.capture({
       event: 'test',
       properties: {
         key: 'value',
       },
     });
-  }
-
-  public async flush(): Promise<void> {
-    await Posthog.flush();
-  }
-
-  public async identify(): Promise<void> {
-    await Posthog.identify({
-      distinctId: 'distinctId',
+    */
+    posthog.capture('test', {
+      key: 'value',
     });
   }
 
+  public async flush(): Promise<void> {
+    /*
+    await Posthog.flush();
+    */
+  }
+
+  public async identify(): Promise<void> {
+    /*
+    await Posthog.identify({
+      distinctId: 'distinctId',
+    });
+    */
+  }
+
   public async register(): Promise<void> {
+    /*
     await Posthog.register({
       key: 'key',
       value: 'value',
     });
+    */
   }
 
   public async reset(): Promise<void> {
-    await Posthog.reset();
+    await posthog.reset();
   }
 
   public async screen(): Promise<void> {
+    /*
     await Posthog.screen({
       screenTitle: 'screenTitle',
       properties: {
         key: 'value',
       },
     });
+    */
   }
 
   public async setup(): Promise<void> {
     const apiKey = this.formGroup.get('apiKey')?.value;
     const host = this.formGroup.get('host')?.value;
+
+    /*
     await Posthog.setup({
-      apiKey,
-      host,
+      apiKey: 'phc_go1WG8cYBkAXIYDWJC4BJdtsl7Xhlhc7B6zTKrIMCLp',
+      host: 'https://eu.i.posthog.com',
+    });
+    */
+
+    posthog.init('phc_go1WG8cYBkAXIYDWJC4BJdtsl7Xhlhc7B6zTKrIMCLp', {
+      api_host: 'https://eu.i.posthog.com',
+      person_profiles: 'identified_only',
     });
   }
 
   public async unregister(): Promise<void> {
+    /*
     await Posthog.unregister({
       key: 'key',
     });
+    */
   }
 }
